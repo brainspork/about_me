25d490e04487e419bb16bf76341fb31a6eefb3a7
Anthony Konschuh <silv.amee@gmail.com>
2017-05-10 16:55:20 -0700

    Added scoring

--- app.js
+++ app.js
@@ -1,48 +1,59 @@
 'use strict';
 
-var questions = ['Do I know how to ride motorcycles?', 'Is my favorite genre of music pop?', 'Am I an avid gamer?', 'Do I plan on trying my luck in amerture MMA in the future?', 'Have I ever been to another country?'];
-var answers = ['yes', 'no', 'yes', 'yes', 'no'];
-var userResponse = [ask];
-
+//user info
 var user = prompt('Hi! What\'s your name?');
 console.log('User:', user);
 alert('Nice to meet you ' + user + ', I hope you like guessing games.');
+var score = 0;
+
+//questions 1-5
+var questions = ['Do I know how to ride motorcycles?', 'Is my favorite genre of music pop?', 'Am I an avid gamer?', 'Do I plan on trying my luck in amerture MMA in the future?', 'Have I ever been to another country?'];
+var answers = ['yes', 'no', 'yes', 'yes', 'no'];
 
 for(var i = 0; i < questions.length; i++){
   var ask = prompt(questions[i]).toLowerCase();
   console.log('User Answer ' + [i+1] +':' + ask);
   if(ask === answers[i]){
     alert('Correct!');
+    score++;
   }else{
     alert('Incorrect!');
   }
 }
+console.log(userResponse);
 
+
+//random number guessing
 var num = Math.floor((Math.random()*10)+1);
 var counter = 1;
 var userNum;
 console.log('Random number:', num);
 
-  while(userNum !== num){
-    userNum = parseInt(prompt('I\'m thinking of a number between 1 and 10. What is it?'));
-    if(userNum < num){
-      alert('You guessed to low!');
-      counter++;
-    }else if(userNum > num){
-      alert('You guessed to high!');
-      counter++;
-    }else if(userNum === NaN || userNum === null){
-      alert('Please enter number.');
-      counter++;
-    }
-    if(counter > 4){
-      alert('Better luck next time!');
-      break;
-    }
+while(userNum !== num){
+  userNum = parseInt(prompt('I\'m thinking of a number between 1 and 10. What is it?'));
+  if(userNum < num){
+    alert('You guessed to low!');
+    counter++;
+  }else if(userNum > num){
+    alert('You guessed to high!');
+    counter++;
+  }else if(userNum === NaN || userNum === null){
+    alert('Please enter number.');
+    counter++;
   }
-
+  if(counter > 4){
+    alert('Better luck next time!');
+    break;
+  }
+}
+if(userNum === num){
+  alert('You got it!');
+  score++;
   console.log('Number of attempts:', counter);
+}
 
+
+//Question 7 multiple choice
 var metal = ['iron maiden', 'judas priest','metallica','death'];
 var userMetal;
 var counterTwo = 0;
@@ -63,24 +74,14 @@ while(counterTwo < 7){
   }else{
     alert('You got one! The choices were: Iron Maiden, Judas Priest, Metallica, Death');
     counterTwo+=7;
+    score++;
   }
     if(counterTwo === 6){
       alert('Better luck next time! The choices were: Iron Maiden, Judas Priest, Metallica, Death');
       break;
   }
 }
-
 console.log('Attempts:', counterTwo);
 
 
-
-
-
-
-
-
-
-
-
-
-'Name one of my favorite metal bands.'
+alert('Congradulations! You finished! You got '+score+' out of 7!');
